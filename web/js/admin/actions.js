55de176cd87da39263fc7af5e2c67b7c8dd3dff4
sylvain <dupont.sylvain59@gmail.com>
2016-11-07 16:57:21 +0100

    correctif leftMenu

--- web/js/admin/actions.js
+++ web/js/admin/actions.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
        $(this).material_select();
    });
    $('.button-collapse').sideNav({
-        menuWidth: 300, // Default is 240
-        edge: 'right', // Choose the horizontal origin
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        menuWidth: 250, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens
       }
     );
